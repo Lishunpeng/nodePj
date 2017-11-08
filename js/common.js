39263e5711caa5a1bdc8db56de911ec5cb7337d6
李顺鹏 <569133352@qq.com>
2017-11-08 18:00:43 +0800

    52454

--- js/common.js
+++ js/common.js
@@ -1,43 +1,56 @@
-var myFun = function(){
-	
-}
+var numAl = /^[0-9a-zA_Z]+$/;
+
+var myFun = function() {}
 myFun.prototype = {
-	//登录页面
-	loginSmb:function(){
-		
-	},
-	//注册页面
-	regiSmb:function(){
-		
-	},
-	
-	
+	//post 提交ajax
+	postajax: function(path, mydata) {
+		mui.ajax({
+			type: "post",
+			url: path,
+			data: mydata,
+			success: function(data) {
+				mui.alert(data);
+			}
+		});
+	}
 }
-
-
-var vm1 = new Vue({})
-console.log(vm1);
-/*
-$("#login").on("click", function() {
-					var acco = $("#acco").val();
-					var opassword = $("#password").val();
-					var data = {
-						myacco: acco,
-						mypassword: opassword
-					}
-					console.log(data)
-					postajax("/mylogin", data);
-});
-
-				function postajax(path, mydata) {
-					$.ajax({
-						type: "post",
-						url: path,
-						data: mydata,
-						success: function(data) {
-							$(".test").text(data);
-						}
-					});
+var myobj = new myFun();
+var vm = new Vue({
+	el: '#login,#firstLogin',
+	data: {
+		opassword:"",
+		opasswordag:"",
+		acco:""
+	},
+	methods: {
+		//登录页面
+		loginSmb: function(obj) {
+			console.log(this.opassword);
+			console.log(this.opasswordag);
+			if(!this.opassword || !this.acco) {
+				return mui.toast("账号和密码不为空")
+			}
+			else if (!numAl.test(this.acco)) {
+				return	mui.toast("账号请用字母加数字");
+			}
+			else if(obj == 0) {
+				if(!this.opasswordag) {
+				return	mui.toast("账号和密码不为空")
+				}else if(this.opasswordag != this.opassword){
+					return	mui.toast("两次密码不正确");
 				}
-				
-*/
+			}
+			var data = {
+				myacco: vm.acco,
+				mypassword: vm.opassword
+			}
+			console.log(data)
+			if(obj == 1) {
+				myobj.postajax("/mylogin", data);
+			} else {
+				myobj.postajax("/firstlogin", data);
+			}
+
+		},
+	},
+});
